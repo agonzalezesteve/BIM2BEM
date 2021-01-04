38b3d719eee470e32dce404ddf3c3af75f0406dd
agonzalezesteve <adria@zeroconsulting.com>
2021-01-04 18:26:41 +0100

    Recovered CGAL clipping algorithm.

--- BIM2BEM.py
+++ BIM2BEM.py
@@ -9,7 +9,6 @@ import ifcopenshell
 import ifcopenshell.geom as geom
 from skgeom import *
 import skgeom
-import pyclipper
 
 settings = ifcopenshell.geom.settings()
 settings.set(settings.USE_WORLD_COORDS, True)
@@ -78,50 +77,17 @@ for building_element in file.by_type('IfcBuildingElement'):
   
   building_element2polyhedron_3[building_element] = polyhedron
 
-SCALING_FACTOR = 1e3
-
-def get_clipper(subjects, clips):
-  clipper = pyclipper.Pyclipper()
-  
-  clipper.AddPaths(pyclipper.scale_to_clipper(subjects, SCALING_FACTOR), pyclipper.PT_SUBJECT, True)
-  clipper.AddPaths(pyclipper.scale_to_clipper(clips, SCALING_FACTOR), pyclipper.PT_CLIP, True)
-  
-  return clipper
-
-def execute_clipping(clipper, type):
-  return pyclipper.scale_from_clipper(clipper.Execute(type, pyclipper.PFT_EVENODD, pyclipper.PFT_EVENODD), SCALING_FACTOR)
-
-def clipping(subjects, clips, operation):
-  clipper = get_clipper(subjects, clips)
-  
-  if operation == "union": return execute_clipping(clipper, pyclipper.CT_UNION)
-  if operation == "intersection": return execute_clipping(clipper, pyclipper.CT_INTERSECTION)
-  if operation == "difference": return execute_clipping(clipper, pyclipper.CT_DIFFERENCE)
-  
-  union = execute_clipping(clipper, pyclipper.CT_UNION)
-  intersection = execute_clipping(clipper, pyclipper.CT_INTERSECTION)
-  
-  if not intersection: return union
-  
-  clipper = get_clipper(union, intersection)
-  
-  return execute_clipping(clipper, pyclipper.CT_DIFFERENCE)
-
 for plane, polygons in plane2polygons.items():
-  solution = [polygons[0].coords.tolist()]
-  for polygon in polygons[1:]:
-    solution = clipping(solution, [polygon.coords.tolist()], "xor")
-    
+  solution = skgeom.PolygonSet()
+  for polygon in polygons:
+    solution = solution.symmetric_difference(polygon)
+  
   print([plane.a(), plane.b(), plane.c(), plane.d()])
-  for polygon in solution:
-    print(list(map(lambda vertex: plane.to_3d(Point2(vertex[0], vertex[1])), polygon)))
-  print("")
-  # print([plane.a(), plane.b(), plane.c(), plane.d()])
-  # for polygon in solution.polygons:
-    # for vertex in polygon.outer_boundary().coords:
-      # print(Point2(vertex[0], vertex[1]))
+  for polygon in solution.polygons:
+    for vertex in polygon.outer_boundary().coords:
+      print(Point2(vertex[0], vertex[1]))
       # print(plane.to_3d(Point2(vertex[0], vertex[1])))
-    # print("")
-  # print("")
+    print("")
+  print("")
   
   plane2polygons[plane] = solution
