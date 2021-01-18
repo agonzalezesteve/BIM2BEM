579828e241c4f19768c1c779cd466d1cc9e54592
agonzalezesteve <adria@zeroconsulting.com>
2021-01-18 15:38:24 +0100

    Started creating IfcSpaces.

--- BIM2BEM.py
+++ BIM2BEM.py
@@ -81,13 +81,12 @@ def clipping(subject, clip, boolean_operation):
 settings = ifcopenshell.geom.settings()
 settings.set(settings.USE_WORLD_COORDS, True)
 
-file = ifcopenshell.open('./test.ifc')
+ifc_file = ifcopenshell.open('./test.ifc')
 
-building_element2faces = {}
 building_element2poly_trees = {}
-for building_element in file.by_type('IfcBuildingElement'):
+for building_element in ifc_file.by_type('IfcBuildingElement'):
   if not (building_element.is_a('IfcWall') or building_element.is_a('IfcSlab')): continue
-  
+    
   shape = ifcopenshell.geom.create_shape(settings, building_element)
   geo = shape.geometry
   vertices = geo.verts
@@ -104,49 +103,52 @@ for building_element in file.by_type('IfcBuildingElement'):
     poly_trees = [pyclipper.PyPolyNode(), pyclipper.PyPolyNode()]
     for building_element_plane, building_element_poly_trees in plane2poly_trees.items():
       is_opposite = are_equal(building_element_plane, plane.opposite())
-      if are_equal(building_element_plane, plane) or is_opposite:
-        plane = building_element_plane
-        poly_trees = building_element_poly_trees
-        break
+      if not (are_equal(building_element_plane, plane) or is_opposite): continue
+      
+      plane = building_element_plane
+      poly_trees = building_element_poly_trees
+      break
     index = (0,1)[is_opposite]
     poly_trees[index] = clipping(poly_trees[index], get_poly_tree(plane, points), "join")
 
     plane2poly_trees[plane] = poly_trees
   
-  building_element2faces[building_element] = plane2poly_trees
-  building_element2poly_trees[building_element] = {plane: clipping(poly_trees[0], poly_trees[1], "symmetric_difference") for plane, poly_trees in plane2poly_trees.items()}
+  building_element2poly_trees[building_element] = plane2poly_trees
 
-# for building_element, building_element_plane2poly_tree in building_element2poly_trees.items():
+# for building_element, plane2poly_trees in building_element2poly_trees.items():
   # print(building_element)
-  # for plane, poly_tree in building_element_plane2poly_tree.items():
+  # for plane, poly_trees in plane2poly_trees.items():
     # print(plane)
+    # poly_tree = clipping(poly_trees[0], poly_trees[1], "symmetric_difference")
     # for polygon2 in pyclipper.PolyTreeToPaths(poly_tree):
       # print(list(map(lambda coord: plane.to_3d(skgeom.Point2(coord[0] / SCALING_FACTOR, coord[1] / SCALING_FACTOR)), polygon2)))
 
 # print(hola)
       
 plane2poly_tree = {}
-for building_element, building_element_plane2poly_tree in building_element2poly_trees.items():
-  for plane, poly_tree in building_element_plane2poly_tree.items():
+for building_element, plane2poly_trees in building_element2poly_trees.items():
+  for plane, poly_trees in plane2poly_trees.items():
+    poly_tree = clipping(poly_trees[0], poly_trees[1], "symmetric_difference")
     for global_plane, global_poly_tree in plane2poly_tree.items():
       is_opposite = are_equal(global_plane, plane.opposite())
-      if are_equal(global_plane, plane) or is_opposite:
-        poly_tree = clipping(global_poly_tree, poly_tree, "symmetric_difference")
-        plane = global_plane
-        break
+      if not (are_equal(global_plane, plane) or is_opposite): continue
+      
+      poly_tree = clipping(global_poly_tree, poly_tree, "symmetric_difference")
+      plane = global_plane
+      break
         
     plane2poly_tree[plane] = poly_tree
     
-count = 0
-for plane, poly_tree in plane2poly_tree.items():
-  print(count)
-  print(plane)
-  for polygon2 in pyclipper.PolyTreeToPaths(poly_tree):
-    print(list(map(lambda coord: plane.to_3d(skgeom.Point2(coord[0] / SCALING_FACTOR, coord[1] / SCALING_FACTOR)), polygon2)))
-  print("")
-  count += 1
+# count = 0
+# for plane, poly_tree in plane2poly_tree.items():
+  # print(count)
+  # print(plane)
+  # for polygon2 in pyclipper.PolyTreeToPaths(poly_tree):
+    # print(list(map(lambda coord: plane.to_3d(skgeom.Point2(coord[0] / SCALING_FACTOR, coord[1] / SCALING_FACTOR)), polygon2)))
+  # print("")
+  # count += 1
 
-print(hola)
+# print(hola)
 
 def path2polygon3(path, plane):
   return list(map(lambda coord: plane.to_3d(skgeom.Point2(coord[0] / SCALING_FACTOR, coord[1] / SCALING_FACTOR)), path))
@@ -234,30 +236,54 @@ for id_polygon_i, polygon3_i in enumerate(firsts):
     
 n_components, labels = connected_components(csgraph=csr_matrix((np.array(data), (np.array(row), np.array(col))), shape=(len(firsts), len(firsts))), directed=False, return_labels=True)
 
-print(n_components)
-print(labels)
+owner_history = ifc_file.by_type("IfcOwnerHistory")[0]
+context = ifc_file.by_type("IfcGeometricRepresentationContext")[0]
 
-# for i in range(0, n_components):
-  # for id, first in enumerate(firsts):
-    # if not labels[id] == i: continue
+for i in range(0, n_components):
+  building_storey = None
+  items = []
+  
+  for id, first in enumerate(firsts):
+    if not labels[id] == i: continue
     
-    # first_plane = first[0]
-    # first_poly_tree = get_poly_tree(first_plane, first[1])
-    # first_poly_tree.depth = 1
+    first_plane = first[0]
+        
+    first_poly_tree = get_poly_tree(first_plane, first[1])
+    first_poly_tree.depth = 1
     
-    # for hole in first[2]:
-      # hole_poly_tree = get_poly_tree(first_plane, hole)
+    for hole in first[2]:
+      hole_poly_tree = get_poly_tree(first_plane, hole)
       
-      # hole_poly_tree.Parent = first_poly_tree
-      # hole_poly_tree.IsHole = True
-      # hole_poly_tree.depth = 1
-      # first_poly_tree.depth = 2
+      hole_poly_tree.Parent = first_poly_tree
+      hole_poly_tree.IsHole = True
+      hole_poly_tree.depth = 1
+      first_poly_tree.depth = 2
+    
+    for building_element, plane2poly_trees in building_element2poly_trees.items():
+      for plane, poly_trees in plane2poly_trees.items():
+        for index, poly_tree in enumerate(poly_trees):
+          is_opposite = are_equal(first_plane, opposite_plane)
+          if not (are_equal(global_plane, plane) or is_opposite): continue
+          
+          if clipping(first_poly_tree, poly_trees[0], "intersection").Contour.Area < 1e-6: continue
+          
+          if building_element.is_a('IfcWall'): building_storey = building_element.ContainedInStructure[0].RelatingStructure
+  
+  axis2placement = create_ifcaxis2placement(ifc_file, point, dir1, dir2)
+  placement = ifc_file.createIfcLocalPlacement(building_storey, axis2placement)
+  
+  ifc_file.createIfcLocalPlacement(ifcfile, relative_to=storey_placement)
+          if index == 1: plane.opposite
+          
+        if clipping(first_poly_tree, poly_trees[0], "intersection").Contour.Area < 1e-6: continue
         
-    # for building_element, building_element_plane2poly_tree in building_element2poly_treee.items():
-      # for plane, building_poly_tree in building_element_plane2poly_tree.items():
-        # is_opposite = are_equal(first_plane, plane.opposite())
+    for building_element, building_element_plane2poly_tree in building_element2poly_treee.items():
+      for plane, building_poly_tree in building_element_plane2poly_tree.items():
         
-        # space_boundary_poly_tree = clipping(building_poly_tree, translate_poly_tree(first_poly_tree, None, first_plane, plane, is_opposite), "intersection")
+        
+        space_boundary_poly_tree = clipping(building_poly_tree, translate_poly_tree(first_poly_tree, None, first_plane, plane, is_opposite), "intersection")
+
+  ifc_file.createIfcShapeRepresentation(context, "Body", "Brep", items)
 
 # https://github.com/IfcOpenShell/IfcOpenShell/blob/fcc2b9ee13e0505c617b306fe5e29890855ced5e/src/ifcblenderexport/blenderbim/bim/export_ifc.py#L3357
 # https://github.com/IfcOpenShell/IfcOpenShell/blob/fcc2b9ee13e0505c617b306fe5e29890855ced5e/src/ifcblenderexport/blenderbim/bim/export_ifc.py#L1925
